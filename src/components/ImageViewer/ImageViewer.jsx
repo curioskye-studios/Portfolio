0083baeb6a6e58674bc2304fd3a2a13f5f1caac1
curioskye-studios <curioskye.studios@gmail.com>
2026-06-18 20:24:18 -0500

    fixed image viewer display, revamping code

--- src/components/ImageViewer/ImageViewer.jsx
+++ src/components/ImageViewer/ImageViewer.jsx
@@ -8,102 +8,94 @@ import MinusIcon from '../Icons/MinusIcon';
 import PlusIcon from '../Icons/PlusIcon';
 import ZoomIcon from '../Icons/ZoomIcon';
 
-import { useImageViewer } from './hooks/useImageViewer';
-import { useKeyboard } from './hooks/useKeyboard';
+import { useState } from 'react';
+
+// import { useImageViewer } from './hooks/useImageViewer';
+// import { useKeyboard } from './hooks/useKeyboard';
 
 export default function ImageViewer({ imgPath, alt = '', caption, className = '' }) {
-  
-  const viewerHook = useImageViewer();
-  useKeyboard(viewerHook.isOpen, viewerHook.closeModal);
 
+  const [isOpen, setIsOpen] = useState(false); 
 
-  useEffect(() => {
-    if (!viewerHook.isOpen) return;
-
-    const imgMain = document.querySelector('.viewer-img-main');
-    if (!imgMain) return;
-
-    imgMain.addEventListener('wheel', viewerHook.onWheel, { passive: false });
-    imgMain.addEventListener('touchmove', viewerHook.onTouchMove, { passive: false });
-
-    return () => {
-      imgMain.removeEventListener('wheel', viewerHook.onWheel);
-      imgMain.removeEventListener('touchmove', viewerHook.onTouchMove);
-    };
-  }, [viewerHook.isOpen, viewerHook.onWheel, viewerHook.onTouchMove]);
-
-
-  const imgStyle = {
-    transform: `scale(${viewerHook.scale}) translate(${viewerHook.offset.x / viewerHook.scale}px, ${viewerHook.offset.y / viewerHook.scale}px)`,
-    cursor: viewerHook.scale > 1 
-      ? (viewerHook.isPanning ? 'grabbing' : 'grab') 
-      : 'zoom-in',
-    transition: viewerHook.isPanning ? 'none' : 'transform 0.15s ease',
+  function openModal() {
+    setIsOpen(true);
   };
 
+  function closeModal() {
+    setIsOpen(false);
+  };
+
+  function disableCloseModal(closeEvent) {
+    closeEvent.stopPropagation();
+  }
+  
 
   const imageViewerContent = (
     <>
       <div 
         className="viewer-backdrop" 
-        onClick={viewerHook.closeModal} 
-        onMouseMove={viewerHook.onMouseMove}
-        onMouseUp={viewerHook.onMouseUp}
-        onMouseLeave={viewerHook.onMouseUp}
+        onClick={closeModal} 
+        // onMouseMove={viewerHook.onMouseMove}
+        // onMouseUp={viewerHook.onMouseUp}
+        // onMouseLeave={viewerHook.onMouseUp}
       >
-        <div className="viewer-modal" onClick={(e) => e.stopPropagation()}>
-          <div className="viewer-header">
+
+        <div className="viewer-modal" onClick={(e) => disableCloseModal(e)}>
+
+          <div className="viewer-header">  
+                    
+            <div className='viewer-caption-wrap'>            
+              <span className="viewer-caption">
+                { caption ? caption : alt }  
+              </span>
+            </div> 
+
             <IconButton 
               svgIconComponent={ <CloseIcon className="viewer-close-icon" /> }
               className="btn-primary viewer-close viewer-btn" 
-              onClick={viewerHook.closeModal}
-              aria-label="Close viewer"
+              onClick={closeModal}
+              title="Close image viewer"
             />
-          </div>                   
+          </div>               
           
           <div 
             className="viewer-img-main" 
-            onMouseDown={viewerHook.onMouseDown}
-            onTouchStart={viewerHook.onTouchStart}
-            onTouchEnd={viewerHook.onTouchEnd}
+            // onMouseDown={viewerHook.onMouseDown}
+            // onTouchStart={viewerHook.onTouchStart}
+            // onTouchEnd={viewerHook.onTouchEnd}
           >
             <img 
-              ref={viewerHook.imgRef} 
+              // ref={viewerHook.imgRef} 
               className="viewer-img"
               src={imgPath}
               alt={alt} 
-              style={imgStyle}
+              // style={imgStyle}
               draggable={false}                 
             />
-          </div>
-                     
-          <div className='viewer-caption-wrap'>
-            <span className="viewer-caption">
-              { caption ? caption : alt }  
-            </span>
-          </div> 
+          </div>        
                      
           <div className="viewer-controls">      
             <IconButton 
               svgIconComponent={ <MinusIcon /> }
               className="btn-primary viewer-btn" 
-              aria-label="Zoom out"
-              onClick={viewerHook.handleZoomOut}
+              title="Zoom out"
+              // onClick={viewerHook.handleZoomOut}
             />
 
             <button 
               className="zoom-tracker" 
-              aria-label="Reset zoom to 100%"
-              onClick={viewerHook.handleZoomReset}
+              title="Reset zoom to 100%"
+              // onClick={viewerHook.handleZoomReset}
             >
-              {Math.round(viewerHook.scale * 100)}%
+              {/* {Math.round(viewerHook.scale * 100)}% */}
+              100%
             </button>   
 
             <IconButton 
               svgIconComponent={ <PlusIcon /> }
               className="btn-primary viewer-btn" 
-              aria-label="Zoom in"
-              onClick={viewerHook.handleZoomIn}
+              title="Zoom in"
+              // onClick={viewerHook.handleZoomIn}
             />
           </div>
 
@@ -117,7 +109,7 @@ export default function ImageViewer({ imgPath, alt = '', caption, className = ''
     <>
       <div 
         className={`thumbnail-wrap ${className}`} 
-        onClick={viewerHook.openModal} 
+        onClick={openModal} 
         title="Click to expand"
       >
         <img className="thumbnail" src={imgPath} alt={alt} />
@@ -127,7 +119,7 @@ export default function ImageViewer({ imgPath, alt = '', caption, className = ''
       </div>
 
       {
-        viewerHook.isOpen ? 
+        isOpen ? 
           createPortal(imageViewerContent, document.body)
           : null
       }
